ec2b70874b2ad52f471886121c3a52d98a0fa945
ignacy125 <ignacy125@gmail.com>
2016-11-24 10:19:55 +0100

    formular with post and get method

--- lekcjapost.go
+++ lekcjapost.go
@@ -12,12 +12,17 @@ func witam(wysylacz http.ResponseWriter, pytanie *http.Request) {
 
 }
 //TODO zmien nazwy zmiennych wysylacz i pytanie na response i request
-func logowanie(wysylacz http.ResponseWriter, pytanie *http.Request) {
-	if(pytanie.Method == "POST") {
+func logowanie(response http.ResponseWriter, request *http.Request) {
+	if(request.Method == "POST") {
 		//TODO Zapisz login i haslo do zmiennych np: login := pytanie.Form["login"] i nie wyswietlaj na konsoli hasla
-		pytanie.ParseForm()
-		fmt.Println("login", pytanie.Form["login"])
-		fmt.Println("pass", pytanie.Form["pass"])
+		request.ParseForm()
+		fmt.Println("login", request.Form["login"])
+		fmt.Println("pass", request.Form["pass"])
+
+		var correctPassword = 12345
+		var correctLogin = "aziron"
+
+		if
 		//TODO Stworz zmienne correctPassword i correctLogin,
 		//TODO Przypisz im wartosci ktore sobie wymyslisz aby za ich pomoca mozna bylo sie zalogowac na strone
 		//TODO Za pomoca warunku if sprawdz czy podany w formularzu login i haslo pasuja do correctPassword itd...
@@ -32,17 +37,28 @@ func logowanie(wysylacz http.ResponseWriter, pytanie *http.Request) {
 	}
 	fmt.Println("Witam na mojej stronie")
 	//fmt.Fprintf(wysylacz,"Witaj")
-	http.ServeFile(wysylacz, pytanie, "logowanie.html")
+	http.ServeFile(response, request, "logowanie.html")
+
+}
+
+func internalHander(wysylacz http.ResponseWriter, pytanie *http.Request) {
+	fmt.Println("Witam na mojej stronie 2")
+	http.ServeFile(wysylacz, pytanie, "internal")
+
+
+
 
 }
 func main(){
 	http.HandleFunc("/", witam)
+
 	//TODO podobnie jak /logowanie stworz nowa strone /internal a obsluge tej strony zapisz w funkcji internalHandler
 	//TODO mozesz skorzystac z http.ServeFile tak jak w funkcji "witam"
 	http.HandleFunc("/logowanie", logowanie)
+	http.HandleFunc("/internal")
 	//TODO Zmien nazwe zmiennej error, poniewaz nazwa koliduje ze zmienna error wbudowana w jezyk golang
-	error := http.ListenAndServe("localhost:5555", nil)
-	if(error != nil){
+	err := http.ListenAndServe("localhost:5555", nil)
+	if(err != nil){
 		println(error)
 	}
 }
