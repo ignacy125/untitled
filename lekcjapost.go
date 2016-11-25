3905094aa0616757e1776e5fbc750941e051744a
aziron Ernest Biela <ernest.biela@gmail.com>
2016-11-25 13:45:26 +0100

    review zadania na zajecia 25.11.2016 17:30

--- lekcjapost.go
+++ lekcjapost.go
@@ -5,24 +5,24 @@ import (
 
 	"fmt"
 )
-
+//TODO tutaj też zmien na response i request
 func witam(wysylacz http.ResponseWriter, pytanie *http.Request) {
 	fmt.Println("Witam na mojej stronie")
 	http.ServeFile(wysylacz, pytanie, "logowanie.html")
 
 }
-//TODO zmien nazwy zmiennych wysylacz i pytanie na response i request
 func logowanie(response http.ResponseWriter, request *http.Request) {
 	if(request.Method == "POST") {
 		//TODO Zapisz login i haslo do zmiennych np: login := pytanie.Form["login"] i nie wyswietlaj na konsoli hasla
+		//TODO nie zapisałeś hasła i loginu do zmiennych, a hasło nadal jest wyświetlana na konsoli
 		request.ParseForm()
 		fmt.Println("login", request.Form["login"])
 		fmt.Println("pass", request.Form["pass"])
-
+		//TODO tak jak pisałem w smsie, brakuje operatora przypisania ":="
 		var correctPassword = 12345
 		var correctLogin = "aziron"
 
-		if
+		if //TODO tego if'a możesz zakomentować jeśli nie wiesz jak go dokończyć
 		//TODO Stworz zmienne correctPassword i correctLogin,
 		//TODO Przypisz im wartosci ktore sobie wymyslisz aby za ich pomoca mozna bylo sie zalogowac na strone
 		//TODO Za pomoca warunku if sprawdz czy podany w formularzu login i haslo pasuja do correctPassword itd...
@@ -40,7 +40,7 @@ func logowanie(response http.ResponseWriter, request *http.Request) {
 	http.ServeFile(response, request, "logowanie.html")
 
 }
-
+//TODO tutaj też zmien na response i request
 func internalHander(wysylacz http.ResponseWriter, pytanie *http.Request) {
 	fmt.Println("Witam na mojej stronie 2")
 	http.ServeFile(wysylacz, pytanie, "internal")
@@ -52,13 +52,12 @@ func internalHander(wysylacz http.ResponseWriter, pytanie *http.Request) {
 func main(){
 	http.HandleFunc("/", witam)
 
-	//TODO podobnie jak /logowanie stworz nowa strone /internal a obsluge tej strony zapisz w funkcji internalHandler
 	//TODO mozesz skorzystac z http.ServeFile tak jak w funkcji "witam"
 	http.HandleFunc("/logowanie", logowanie)
-	http.HandleFunc("/internal")
-	//TODO Zmien nazwe zmiennej error, poniewaz nazwa koliduje ze zmienna error wbudowana w jezyk golang
+	http.HandleFunc("/internal") //TODO brakuje wywołania funkcji "internalHandler", która ma obsłużyć nową stronę, tak jak wyżej "logowanie"
 	err := http.ListenAndServe("localhost:5555", nil)
 	if(err != nil){
+		//TODO Dobrze, ale println teraz nie wyswietli tego błędu, bo używasz starej nazwy zmiennej
 		println(error)
 	}
 }
@@ -66,3 +65,5 @@ func main(){
 
 //TODO Jesli masz jakies pytanie albo czegos nie wiesz, napisz maila do mnie. Czas na wykonanie zadania do czwartku do 20:00
 //TODO Po skonczonej pracy w danym dniu uzyj gita CTRL + k i wrzuc zmiany na githuba
+
+//TODO ogólnie fajnie że dużo zrobiłeś, ale zostawiłeś kod w stanie w którym się nie kompiluje. Lepiej zakomentować te fragmenty :)
