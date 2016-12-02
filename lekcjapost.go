25092589c9d07829db3b5eb60405a2b2d7bc674e
ignacy125 <ignacy125@gmail.com>
2016-12-02 18:26:55 +0100

    obsługa favicon i poprawki

--- lekcjapost.go
+++ lekcjapost.go
@@ -3,7 +3,7 @@ import (
 	"net/http"
 	"fmt"
 	"time"
-	//TODO nieużywana biblioteka, należy ją usunąć
+
 	"strings"
 )
 var login string
@@ -12,20 +12,6 @@ var login string
 func witam(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("Witam na mojej stronie", request.URL.Path)
 
-	//TODO zauważyłeś że komunikat się wyświetla w konsoli 2 razy? Nie wiesz dlaczego tak jest?
-	//TODO wyświetl na konsoli oprócz standardowego powitania wartość request.URL.Path.
-	//TODO Czy te 2 uruchomienia są faktycznie takie same? Jeśli nie, to w funkcji main stwórz taki HandleFunc()
-	//TODO żeby nie wyświetlać 2 razy komunikatu powitalnego na konsoli
-
-
-
-	//TODO podpowiedź: przy pierwszym uruchomieniu ścieżka to "/" katakog główny
-	//TODO przy drugim uruchomieniu ścieżka to "/favicon.ico" (przeglądarka internetowa szuka pliku ikony Twojej strony,
-	//TODO która ma się pojawić na pasku zakładek lub w zapisanych (ulubionych) stronach.
-	//TODO Komunikat z linii 24 wyświetla się na konsoli 2 razy, ponieważ funkcja "witam" uruchamia się przy podaniu
-	//TODO ścieżki "/" oraz każdej innej nie obsłużonej przez HandleFunc() Jest to domyślna ścieżka.
-	//TODO jeśli stworzysz HandleFunc który obsłuży ten przypadek i uruchomisz w nim funkcję która nic nie robi
-	//TODO to komunikat nie będzie już wyświetlany 2 razy
 	http.ServeFile(response, request, "logowanie.html")
 
 }
@@ -42,26 +28,8 @@ func logowanie(response http.ResponseWriter, request *http.Request) {
 		login = request.Form["login"][0]
 		haslo := request.Form["pass"][0]
 
-		/*if strings.EqualFold(login, correctLogin) {
-			if haslo == correctPassword {
-				http.Redirect(response, request, "/internal", 302)
 
-			} else {
-				fmt.Println("Niepoprawne hasło")
-				time.Sleep(2)
-				http.Redirect(response, request, "/invalid_login", 302)
-			}
-
-		} else {
-			fmt.Println("Niepoprawne login")
-			time.Sleep(2)
-			http.Redirect(response, request, "/invalid_login", 302)
-
-		}
-
-		*/
-		//TODO o to chodziło, tylko pamiętaj że do porównywania używamy operatora "==" a nie "=" lub ":="
-		if login == correctLogin && haslo == correctPassword {
+		if strings.EqualFold(login, correctLogin) && haslo == correctPassword {
 			fmt.Println("Zalogowano")
 			http.Redirect(response, request, "/internal", 302)
 		} else {
@@ -96,10 +64,7 @@ func footerHandler(response http.ResponseWriter, request *http.Request) {
 	http.ServeFile(response, request, "templates/footer.html")
 
 }
-/*func internalHandler(response http.ResponseWriter, request *http.Request) {
-	//http.ServeFile(response, request, "templates/internal.html")
 
-} */
 func welcomeHandler(response http.ResponseWriter, request *http.Request) {
 	http.ServeFile(response, request, "templates/welcome.html")
 
@@ -113,9 +78,7 @@ func logoutFormHandler(response http.ResponseWriter, request *http.Request) {
 
 }
 
-func HandleFuncHandler(response http.ResponseWriter, request *http.Request) {
 
-}
 
 func main() {
 	http.HandleFunc("/", witam)
@@ -127,7 +90,7 @@ func main() {
 	http.HandleFunc("/internal", internalHandler)
 	http.HandleFunc("/logoutForm", logoutFormHandler)
 	http.HandleFunc("/welcome", welcomeHandler)
-	http.HandleFunc("/", HandleFuncHandler)
+	http.HandleFunc("/favicon.ico", func (response http.ResponseWriter, request *http.Request) {})
 	err := http.ListenAndServe("localhost:5555", nil)
 	if (err != nil) {
 		println(err)
