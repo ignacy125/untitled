31b2a101dabdfefea64eae19756b1f6771f02d97
ignacy125 <ignacy125@gmail.com>
2017-01-26 22:37:04 +0100

    sesja

--- lekcjapost.go
+++ lekcjapost.go
@@ -5,83 +5,82 @@ import (
 	"time"
 	"strings"
 	"html/template"
-	"sync"
-	"net/url"
-	"io"
-	"encoding/base64"
-	"crypto/rand"
+ 	"github.com/gorilla/sessions"
 )
-var globalSessions *session.Manager
+//var globalSessions *session.Manager
 var login string
 type Dane struct{
 	Title string
 	User string
 
 }
-type Session interface {
-	Set(key, value interface{}) error //set session value
-	Get(key interface{}) interface{}  //get session value
-	Delete(key interface{}) error     //delete session value
-	SessionID() string                //back current sessionID
-}
-type Provider interface {
-	SessionInit(sid string) (Session, error)
-	SessionRead(sid string) (Session, error)
-	SessionDestroy(sid string) error
-	SessionGC(maxLifeTime int64)
-}
-
-type Manager struct {
-	cookieName  string     //private cookiename
-	lock        sync.Mutex // protects session
-	provider    Provider
-	maxlifetime int64
-}
-var provides = make(map[string]Provider)
+//type Session interface {
+//	Set(key, value interface{}) error //set session value
+//	Get(key interface{}) interface{}  //get session value
+//	Delete(key interface{}) error     //delete session value
+//	SessionID() string                //back current sessionID
+//}
+//type Provider interface {
+//	SessionInit(sid string) (Session, error)
+//	SessionRead(sid string) (Session, error)
+//	SessionDestroy(sid string) error
+//	SessionGC(maxLifeTime int64)
+//}
+//
+//type Manager struct {
+//	cookieName  string     //private cookiename
+//	lock        sync.Mutex // protects session
+//	provider    Provider
+//	maxlifetime int64
+//}
+//var provides = make(map[string]Provider)
 
 // Register makes a session provider available by the provided name.
 // If a Register is called twice with the same name or if the driver is nil,
 // it panics.
-func Register(name string, provider Provider) {
-	if provider == nil {
-		panic("session: Register provider is nil")
-	}
-	if _, dup := provides[name]; dup {
-		panic("session: Register called twice for provider " + name)
-	}
-	provides[name] = provider
-}
+//func Register(name string, provider Provider) {
+//	if provider == nil {
+//		panic("session: Register provider is nil")
+//	}
+//	if _, dup := provides[name]; dup {
+//		panic("session: Register called twice for provider " + name)
+//	}
+//	provides[name] = provider
+//}
+//
+//func (manager *Manager) sessionId() string {
+//	b := make([]byte, 32)
+//	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+//		return ""
+//	}
+//	return base64.URLEncoding.EncodeToString(b)
+//}
+//
+//func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
+//	provider, ok := provides[provideName]
+//	if !ok {
+//		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
+//	}
+//	return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
+//}
+//func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
+//	manager.lock.Lock()
+//	defer manager.lock.Unlock()
+//	cookie, err := r.Cookie(manager.cookieName)
+//	if err != nil || cookie.Value == "" {
+//		sid := manager.sessionId()
+//		session, _ = manager.provider.SessionInit(sid)
+//		cookie := http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.maxlifetime)}
+//		http.SetCookie(w, &cookie)
+//	} else {
+//		sid, _ := url.QueryUnescape(cookie.Value)
+//		session, _ = manager.provider.SessionRead(sid)
+//	}
+//	return
+//}
+var store = sessions.NewCookieStore([]byte("ala-ma-kota"))
 
-func (manager *Manager) sessionId() string {
-	b := make([]byte, 32)
-	if _, err := io.ReadFull(rand.Reader, b); err != nil {
-		return ""
-	}
-	return base64.URLEncoding.EncodeToString(b)
-}
 
-func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
-	provider, ok := provides[provideName]
-	if !ok {
-		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
-	}
-	return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
-}
-func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
-	manager.lock.Lock()
-	defer manager.lock.Unlock()
-	cookie, err := r.Cookie(manager.cookieName)
-	if err != nil || cookie.Value == "" {
-		sid := manager.sessionId()
-		session, _ = manager.provider.SessionInit(sid)
-		cookie := http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.maxlifetime)}
-		http.SetCookie(w, &cookie)
-	} else {
-		sid, _ := url.QueryUnescape(cookie.Value)
-		session, _ = manager.provider.SessionRead(sid)
-	}
-	return
-}
 
 func root(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("Witam na mojej stronie", request.URL.Path)
@@ -103,8 +102,12 @@ func logowanie(response http.ResponseWriter, request *http.Request) {
 
 		login = request.Form["login"][0]
 		haslo := request.Form["pass"][0]
-		globalSessions = NewManager("memory","gosessionid",3600)
-		sess := globalSessions.SessionStart(response, request)
+
+		//session, _ := store.Get(request, "logowanie")
+		//session.Values["username"] = login
+
+//		globalSessions = NewManager("memory","gosessionid",3600)
+		/*sess := globalSessions.SessionStart(response, request)
 		request.ParseForm()
 		if request.Method == "GET" {
 			t, _ := template.ParseFiles("login.gtpl")
@@ -114,6 +117,7 @@ func logowanie(response http.ResponseWriter, request *http.Request) {
 			sess.Set("username", request.Form["username"])
 			http.Redirect(response, request, "/", 302)
 		}
+		*/
 
 		if strings.EqualFold(login, correctLogin) && haslo == correctPassword {
 			fmt.Println("Zalogowano")
